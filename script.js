9cb691f98b897616007dd7bef305743be2f96b79
usama razzaq <usamarazzaq2525@gmail.com>
2021-03-25 00:15:56 +0500

    First 4 programs

---
+++ script.js
@@ -0,0 +1,57 @@
+// 1. Write a JavaScript program to list the properties of a JavaScript object
+// var student = 
+// {
+//     name : "Usama Razzaq",
+//     sclass : "Batch-3",
+//     rollno : 18033
+// };
+// document.write("My name is "+student.name+" and I am student of "+student.sclass+" and my roll no is "+student.rollno)
+
+// 2. Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property.
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 }
+// document.write("Before deleting the object:"+"<br>")
+// document.write(JSON.stringify(student))
+// document.write("<br>")
+// document.write("After deleting the object:")
+// document.write("<br>")
+// delete student.rollno
+// document.write(JSON.stringify(student))
+
+// 3. Write a JavaScript program to get the length of a JavaScript object.   
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+// document.write("There are "+Object.keys(student).length+" properties in object.")
+
+// 4. Write a JavaScript program to display the reading status 
+// (ie display book name, author name and reading status) of the following books.   
+// var library = [ 
+//     {
+//         author: 'Bill Gates',
+//         title: 'The Road Ahead',
+//         readingStatus: true},
+//     {
+//         author: 'Steve Jobs',
+//         title: 'Walter Isaacson',
+//         readingStatus: true},
+//     {
+//         author: 'Suzanne Collins',
+//         title: 'Mockingjay: The Final Book of The Hunger Games', 
+//         readingStatus: false
+//     }
+// ]
+// var i ;
+// for(i=0; i<library.length; i++)
+// {
+//     document.write("Author Name: "+library[i].author+", Book Title: "+library[i].title+", ReadingStatus: "+library[i].readingStatus)
+//     document.write("<br>")
+// }
+
+// 5. Write a JavaScript program to get the volume of a Cylinder with 
+// four decimal places using object classes.   
+// Volume of a cylinder : V = πr2h
+// where r is the radius and h is the height of the cylinder.
