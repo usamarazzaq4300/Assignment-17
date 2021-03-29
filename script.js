c85ac64787150dff47affe03e461ee2c048d167b
usama razzaq <usamarazzaq2525@gmail.com>
2021-03-29 23:59:02 +0500

    added program

--- script.js
+++ script.js
@@ -511,5 +511,24 @@
 // var year = prompt("Please enter the year:")
 // numericYear(year)
 
+// 35. Write a JavaScript function to get minutes with leading zeros (00 to 59).   
+// Test Data :
+// dt = new Date(1989, 10, 1);
+// console.log(minutes_with_leading_zeros(dt));
+// "00"
 
-
+function minutes()
+{
+    var date = new Date()
+    var mins = date.getMinutes()
+    if(mins<10)
+    {
+        mins = "0"+mins
+    }
+    else
+    {
+        mins = mins
+    }
+    console.log(mins)
+}
+minutes()
